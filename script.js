de395eff3c87836dbb776c460f9208522aa0dd37
mfaiz2003 <mfaiz76542@gmail.com>
2025-09-08 15:21:02 +0530

    Your commit message here

---
+++ script.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll(".slide");
+    let index = 0;
+
+    setInterval(() => {
+      slides[index].classList.remove("active");  
+      index = (index + 1) % slides.length;       
+      slides[index].classList.add("active");     
+    }, 4000);
+
+    function changeColor(){
+        
+        
+      
+    }
+
+   
