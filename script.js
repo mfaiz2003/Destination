a3f743d81f82c1fc969ee7731944051f7626b568
mfaiz2003 <mfaiz76542@gmail.com>
2025-09-17 16:14:29 +0530

    initial

--- script.js
+++ script.js
@@ -18,26 +18,3 @@ const slides = document.querySelectorAll(".slide");
     heading.innerHTML ='Book Your <span class="highlight">Next Trip</span><br>with Us';
   }, 3000);
    
-
-const image = document.getElementById('card-image');
-const video = document.getElementById('card-video');
-
-// 5 sec ke baad video start hoga
-setTimeout(() => {
-  image.style.display = 'none';
-  video.style.display = 'block';
-  video.play();
-}, 5000);
-
-// jab video khatam ho jaaye
-video.addEventListener('ended', () => {
-  video.style.display = 'none';
-  image.style.display = 'block';
-
-  // phir 5 sec ke baad video dobara play hoga
-  setTimeout(() => {
-    image.style.display = 'none';
-    video.style.display = 'block';
-    video.play();
-  }, 5000);
-});
